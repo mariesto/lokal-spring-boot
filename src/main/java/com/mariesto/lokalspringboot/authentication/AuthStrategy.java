ff03d7ae360f00aa616fdad77961441141b8b57c
mariesto <sitinjakamendo@gmail.com>
2024-11-30 18:52:44 +0700

    fix: remove un-used code

--- src/main/java/com/mariesto/lokalspringboot/authentication/AuthStrategy.java
+++
@@ -1,7 +0,0 @@
-package com.mariesto.lokalspringboot.authentication;
-
-import org.springframework.http.HttpHeaders;
-
-public interface AuthStrategy {
-    void applyAuthentication(HttpHeaders headers);
-}
